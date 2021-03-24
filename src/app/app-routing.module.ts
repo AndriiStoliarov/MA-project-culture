0892d2a2067d05eb5687d706fb720ad06a7ee3a1
LightWarrior <andrey159852@gmail.com>
2021-03-24 13:34:45 +0200

    created main pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { PosterPageComponent } from './poster-page/poster-page.component';
+import { MainLayoutComponent } from './shared/components';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'home', component: HomePageComponent},
+      {path: 'post/:id', component: PostPageComponent},
+      {path: 'poster', component: PosterPageComponent},
+      {path: 'about', component: AboutPageComponent},
+      {path: '**', redirectTo: 'home'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PosterPageComponent } from './poster-page/poster-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PosterPageComponent,
+    AboutPageComponent,
+    PostPageComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
