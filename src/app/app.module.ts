498dc7307132ebd97fac448b3470cfe9d6b2e3b3
LightWarrior <andrey159852@gmail.com>
2021-03-30 15:10:37 +0300

    edited module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './shared/components';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PosterPageComponent } from './poster-page/poster-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
+import { MainLayoutComponent } from './shared/components';
 import { PostComponent } from './shared/components';
 import { RegistrationPageComponent } from './shared/components';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
