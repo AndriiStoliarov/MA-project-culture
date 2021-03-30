c3a826422d4ee93185eda4aba2a9004343cbce99
LightWarrior <andrey159852@gmail.com>
2021-03-30 13:22:26 +0300

    confiqurated tslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PosterPageComponent } from './poster-page/poster-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
-import { PostComponent } from './shared/components/post/post.component';
+import { MainLayoutComponent } from './shared/components';
+import { PostComponent } from './shared/components';
 
 @NgModule({
   declarations: [
