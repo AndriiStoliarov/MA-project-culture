bc982e961f22bfbd8c238622157d7741dd33fa46
LightWarrior <andrey159852@gmail.com>
2021-04-09 18:19:39 +0300

    angular material for navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+// import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -41,7 +43,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatSliderModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
