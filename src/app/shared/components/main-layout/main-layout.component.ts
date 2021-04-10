a8ba083ecf990fa4c7c1c816bb0c52ad9d9a1a26
LightWarrior <andrey159852@gmail.com>
2021-04-10 22:31:40 +0300

    stylized registration form

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -8,50 +8,10 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) { }
-
-  registrationForm: any;
+  constructor() { }
 
   ngOnInit(): void {
-    this.registrationForm = this.fb.group ({
-      name: new FormControl('', [
-        Validators.required
-      ]),
-      phone: new FormControl('', [
-        Validators.required,
-        Validators.pattern(/^\d{10}$/)
-      ]),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.email
-      ]),
-      password: new FormControl('', [
-        Validators.required
-      ]),
-    });
+
   }
 
-  isControlInvalid(fieldName: string): boolean {
-    return (this.registrationForm.get(fieldName).invalid
-      && (this.registrationForm.get(fieldName).dirty
-      || this.registrationForm.get(fieldName).touched));
-  }
-
-  getControlName(controlName: string): boolean {
-    return this.registrationForm.get(controlName);
-  }
-
-  getContolError(controlName: string): string | null {
-    const control = this.registrationForm.get(controlName);
-    if (control.errors.required) {
-      return 'The field cannot be empty';
-    }
-    if (control.errors.email) {
-      return 'Enter correct email';
-    }
-    if (control.errors.patter !== /^\d{10}$/) {
-      return 'Enter the 10-digit number';
-    }
-    return null;
-  }
 }
