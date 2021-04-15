91aba61255a4f5c51b175906a38ef92294af3155
LightWarrior <andrey159852@gmail.com>
2021-04-15 13:51:25 +0300

    routing for button event

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { EventFormComponent } from '../event-form/event-form.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { UserLayoutComponent } from './shared/components/user-layout/user-layout.component';
 import { UserPostsPageComponent } from './user-posts-page/user-posts-page.component';
@@ -8,7 +9,8 @@ export const userRouting: Routes = [
     path: '', component: UserLayoutComponent, children: [
       {path: '', redirectTo: '/user/login', pathMatch: 'full'},
       {path: 'login', component: LoginPageComponent},
-      {path: 'posts', component: UserPostsPageComponent}
+      {path: 'posts', component: UserPostsPageComponent},
+      {path: 'event', component: EventFormComponent},
     ]
   }
 ];
