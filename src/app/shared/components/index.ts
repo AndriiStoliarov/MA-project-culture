e255aa8f1105c1f9da06fcee0277e867d29c8760
LightWarrior <andrey159852@gmail.com>
2021-04-16 20:55:50 +0300

    created need post

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -3,3 +3,4 @@ export { PostComponent } from './post/post.component';
 export { RegistrationPageComponent } from './registration-page/registration-page.component';
 export { PostsComponent } from './posts/posts.component';
 export { NeedFormComponent } from './need-form/need-form.component';
+export { NeedPostComponent } from './need-post/need-post.component';
