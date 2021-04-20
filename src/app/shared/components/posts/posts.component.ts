4734db0c8937d3c6f1aa908ed806462463698e88
LightWarrior <andrey159852@gmail.com>
2021-04-20 14:50:32 +0300

    added pagination

--- src/app/shared/components/posts/posts.component.ts
+++ src/app/shared/components/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
+  length: number = null;
+  pageSize = 6;
+  pageIndex = 0;
+  pageSizeOptions = [3, 6, 12, 48];
+  showFirstLastButtons = true;
+
+  public posts = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+  ];
+
+  public pageSlice = this.posts.slice(0, this.pageSize);
+
   constructor() { }
 
   ngOnInit(): void {
+    this.length = this.posts.length;
+  }
+
+  handlePageEvent(event: PageEvent): void {
+    this.length = event.length;
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+
+    if (endIndex > this.posts.length) {
+      endIndex = this.posts.length;
+    }
+
+    this.pageSlice = this.posts.slice(startIndex, endIndex);
   }
 
 }
